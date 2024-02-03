f2650c903acb13320586f600f207860a8c3b97ed
bete7512 <betekbebe@gmail.com>
2024-02-03 12:39:25 +0300

    Add Docker commands to makefile and update songRoutes.js

--- backend/src/api/routes/songRoutes.js
+++ backend/src/api/routes/songRoutes.js
@@ -3,7 +3,30 @@ import express from 'express';
 import * as songController from '../controllers/songController';
 
 const router = express.Router();
+// addissmongouser
+// mr1dq9jzLuB9WQFz
+// mongodb+srv://addissmongouser:mr1dq9jzLuB9WQFz@cluster0.ddj0hxi.mongodb.net/?retryWrites=true&w=majority
+/*
 
+const mongoose = require('mongoose');
+const uri = "mongodb+srv://<username>:<password>@cluster0.ddj0hxi.mongodb.net/?retryWrites=true&w=majority";
+
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
+async function run() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+  }
+}
+run().catch(console.dir);
+
+*/
 router.get('/analytics', songController.generateAnalytics)
 router.get('/songs/:id', songController.getSong);
 router.get('/songs', songController.listSongs);
